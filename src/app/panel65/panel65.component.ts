5f71b22cba2777f340a1086dc0c6021d65963827
EashenMahmud <eehssan7@gmail.com>
2023-08-22 15:18:39 +0600

    OK

---
+++ src/app/panel65/panel65.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel65',
+  templateUrl: './panel65.component.html',
+  styleUrls: ['./panel65.component.css']
+})
+export class Panel65Component {
+
+}
---
+++ src/app/signage/signage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signage',
+  templateUrl: './signage.component.html',
+  styleUrls: ['./signage.component.css']
+})
+export class SignageComponent {
+
+}
